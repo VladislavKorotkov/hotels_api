154f6fe541676bbd5d6ab1d74cd4a8ea88d9a738
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-06-03 19:05:06 +0300

    Initial commit

---
+++ src/main/java/com/korotkov/hotelsapi/HotelsapiApplication.java
@@ -0,0 +1,13 @@
+package com.korotkov.hotelsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korotkov/hotelsapi/HotelsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korotkov.hotelsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
