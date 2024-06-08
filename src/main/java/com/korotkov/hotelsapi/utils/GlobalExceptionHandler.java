ea065229ad9cdb760261b8225a587bbd59e5dc70
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-06-08 23:22:33 +0300

    add ObjectNotFoundException

--- src/main/java/com/korotkov/hotelsapi/utils/GlobalExceptionHandler.java
+++ src/main/java/com/korotkov/hotelsapi/utils/GlobalExceptionHandler.java
@@ -1,25 +1,51 @@
 package com.korotkov.hotelsapi.utils;
 
+import com.korotkov.hotelsapi.exceptions.ObjectNotFoundException;
 import com.korotkov.hotelsapi.responses.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleAllExceptions(Exception ex, WebRequest request) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest req) {
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    return fieldName + ": " + errorMessage;
+                })
+                .collect(Collectors.toList());
+
         ApiError apiError = new ApiError(
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                ex.getMessage(),
-                Arrays.asList("Internal server error")
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation failed",
+                errors
         );
-        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<ApiError> handleObjectNotFoundException(ObjectNotFoundException ex) {
+        ApiError apiError = new ApiError(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                List.of("Object not found")
+        );
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
 
 }
